b8b8b1e744267eecf027b3700b66ada911371739
ramzoon <mirae1jun@gmail.com>
2022-12-05 15:58:42 +0900

    seperate constant

--- src/App.tsx
+++ src/App.tsx
@@ -29,34 +29,6 @@ function App() {
   };
 
   const KeyButtonList = () => {
-    const alphas = [
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F',
-      'G',
-      'H',
-      'I',
-      'J',
-      'K',
-      'L',
-      'M',
-      'N',
-      'O',
-      'P',
-      'Q',
-      'R',
-      'S',
-      'T',
-      'U',
-      'V',
-      'W',
-      'X',
-      'Y',
-      'Z',
-    ];
     const buttonList = alphas.map((alpha) => (
       <KeyButton style={{ fontFamily: font }} onClick={() => onKeyClick(alpha)}>
         {alpha}
